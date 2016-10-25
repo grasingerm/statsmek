21e7eefb0d55681d9e5aadd971bc5a6d24a0e871
Matthew Grasinger <grasingerm@gmail.com>
2016-10-24 23:01:12 -0400

    made corrections to assignment 2

--- wk2/assignment/B3_2.py
+++ wk2/assignment/B3_2.py
@@ -15,30 +15,24 @@ def pair_time(pos_a, vel_a, pos_b, vel_b, sigma):
     del_v = [vel_b[0] - vel_a[0], vel_b[1] - vel_a[1]]
     del_v_sq = del_v[0] ** 2 + del_v[1] ** 2
     scal = del_v[0] * del_x[0] + del_v[1] * del_x[1]
-    Upsilon = scal ** 2 - del_v_sq * ( del_x_sq - 4.0 * sigma **2)
+    Upsilon = scal ** 2 - del_v_sq * ( del_x_sq - 4.0 * sigma **2
+)
     if Upsilon > 0.0 and scal < 0.0:
         del_t = - (scal + math.sqrt(Upsilon)) / del_v_sq
     else:
         del_t = float('inf')
     return del_t
 
-conf_a = ((0.30, 0.30), (0.30, 0.70), (0.70, 0.30), (0.70,0.70))
-conf_b = ((0.20, 0.20), (0.20, 0.80), (0.75, 0.25), (0.75,0.75))
-conf_c = ((0.30, 0.20), (0.30, 0.80), (0.70, 0.20), (0.70,0.70))
-configurations = [conf_a, conf_b, conf_c]
-hits = {conf_a: 0, conf_b: 0, conf_c: 0}
-del_xy = 0.10
 pos = [[0.25, 0.25], [0.75, 0.25], [0.25, 0.75], [0.75, 0.75]]
 vel = [[0.21, 0.12], [0.71, 0.18], [-0.23, -0.79], [0.78, 0.1177]]
 singles = [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)]
 pairs = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
-histo_data = []
 sigma = 0.1197
-t = 0.0
-n_events = 5000000
+t = 0.1
+n_events = 1000000
+histo_data = []
 for event in range(n_events):
-    if event % 100000 == 0:
-        print 'event, t = ', event, ', ', t
+    if event %20000 == 0: print event
     wall_times = [wall_time(pos[k][l], vel[k][l], sigma) for k, l  in singles]
     pair_times = [pair_time(pos[k], vel[k], pos[l], vel[l], sigma) for k, l in pairs]
     next_event = min(wall_times + pair_times)
@@ -48,12 +42,11 @@ for event in range(n_events):
         for k, l in singles:
             pos[k][l] += vel[k][l] * del_t
         t_previous = inter_times
-        for k in range(4): 
-            histo_data.append(pos[k][0])
+        for k in range(4):   # <=================== ALL disks are considered
+            histo_data.append(pos[k][0])  # <================== ALL x-positions are updated.
     t += next_event
     del_t = t - t_previous
-    for k, l in singles:
-        pos[k][l] += vel[k][l] * del_t
+    for k, l in singles: pos[k][l] += vel[k][l] * del_t
     if min(wall_times) < min(pair_times):
         collision_disk, direction = singles[wall_times.index(next_event)]
         vel[collision_disk][direction] *= -1.0
@@ -67,12 +60,11 @@ for event in range(n_events):
         for k in range(2):
             vel[a][k] += e_perp[k] * scal
             vel[b][k] -= e_perp[k] * scal
-
-print 't_final = ', t
+print 'collected data:',len(histo_data)
 pylab.hist(histo_data, bins=100, normed=True)
+pylab.grid()
 pylab.xlabel('x')
 pylab.ylabel('frequency')
-pylab.title('Event: x coordinate histogram (density eta=0.18)')
-pylab.grid()
-pylab.savefig('event_disks_histo.png')
+pylab.title('Event-driven (equal time): x coordinate histogram (density eta=0.18)')
+pylab.savefig('event_disks_histo-case3.png')
 pylab.show()
