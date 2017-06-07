630fdfa14576f3bc4c3c654e37b6bd3e6645c520
Matthew Grasinger <grasingerm@gmail.com>
2017-06-07 15:20:30 -0400

    finished assignment 6

--- wk6/assignment/c2.py
+++ wk6/assignment/c2.py
@@ -3,27 +3,27 @@ import math, random, pylab
 cubic = -1
 quartic = 1
 
-def V(x, cubic, quartic):
-    pot = x ** 2 / 2.0 + cubic * x ** 3 + quartic * x ** 4
+def V_anharm(x, cubic, quartic):
+    pot = cubic * x ** 3 + quartic * x ** 4
     return pot
 
-def levy_free_path(xstart, xend, dtau, N):
+def levy_harmonic_path(xstart, xend, dtau, N):
     x = [xstart]
     for k in range(1, N):
         dtau_prime = (N - k) * dtau
-        x_mean = (dtau_prime * x[k - 1] + dtau * xend) / (dtau + dtau_prime)
-        sigma = math.sqrt(1.0 / (1.0 / dtau + 1.0 / dtau_prime))
-        x.append(random.gauss(x_mean, sigma))
+        Ups1 = 1.0 / math.tanh(dtau) + 1.0 / math.tanh(dtau_prime)
+        Ups2 = x[k-1] / math.sinh(dtau) + xend / math.sinh(dtau_prime)
+        x.append(random.gauss(Ups2 / Ups1, 1.0 / math.sqrt(Ups1)))
     return x
 
 def weight(x):
     global cubic
     global quartic
-    return math.exp(sum(-V(a, cubic, quartic) * dtau for a in x))
+    return math.exp(sum(-V_anharm(a, cubic, quartic) * dtau for a in x))
 
 beta = 20.0
 N = 100
-Ncut = N / 4
+Ncut = N / 2
 dtau = beta / N
 delta = 1.0
 n_steps = 10000000
@@ -32,7 +32,7 @@ old_weight = weight(x)
 data = []
 
 for step in range(n_steps):
-    x_new = levy_free_path(x[0], x[Ncut], dtau, Ncut) + x[Ncut:]
+    x_new = levy_harmonic_path(x[0], x[Ncut], dtau, Ncut) + x[Ncut:]
     new_weight = weight(x_new)
     if random.uniform(0.0, 1.0) < new_weight / old_weight:
         x = x_new[:]
@@ -44,11 +44,12 @@ for step in range(n_steps):
     if step % 100000 == 0:
         print 'step ', step
 
-pylab.hist(data, normed=True, bins=100)
+final_path = x[:]
+pylab.hist(data, normed=True, bins=100, label='QMC')
 pylab.legend()
 pylab.xlabel('$x$')
 pylab.ylabel('$\\pi(x)$ (normalized)')
 pylab.title('levy_anharmonic_path ($\\beta=%s, N=%i$)' % (beta, N))
-pylab.savefig('c1.png');
+pylab.savefig('c2.png');
 pylab.show()
 pylab.clf()
